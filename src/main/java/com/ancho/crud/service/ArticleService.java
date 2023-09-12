5d7117ff78f305c7f02e1108eb8ecbec5a2d0de2
minju-sin <sinminju04@gmail.com>
2023-09-12 13:14:27 +0900

    #18 - 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/ancho/crud/service/ArticleService.java
+++ src/main/java/com/ancho/crud/service/ArticleService.java
@@ -3,21 +3,20 @@ package com.ancho.crud.service;
 import com.ancho.crud.domain.Article;
 import com.ancho.crud.domain.type.SearchType;
 import com.ancho.crud.dto.ArticleDto;
-import com.ancho.crud.dto.ArticleUpdateDto;
 import com.ancho.crud.dto.ArticleWithCommentsDto;
 import com.ancho.crud.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 /*
 * 게시판 서비스 구현
 */
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -28,27 +27,51 @@ public class ArticleService {
     @Transactional(readOnly = true) // 읽어주는 작업만 할 때 붙여주는 기능
     public Page<ArticleDto> searchArticles(SearchType searchType,
                                            String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        //  검색어 없는 경우 -> 아무 일도 일어나지 않고, 그냥 페이지 보여줌
+        if(searchKeyword == null || searchKeyword.isBlank()){
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        //  검색어 존재하는 경우
+        //  swith 구문 자체를 return 할 수 있다.
+        //  searchKeyword로 해당 검색어를 찾아 return 한다.
+        return switch (searchType){
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccount_UserIdContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
     @Transactional(readOnly = true)
     public ArticleWithCommentsDto getArticle(Long articleId){
-        return null;
-    }
-
-    @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
-        return null;
+        //  게시글 조회
+        return articleRepository.findById(articleId)
+                .map(ArticleWithCommentsDto::from)
+                .orElseThrow(()->new EntityNotFoundException("게시글이 없습니다 - articleId:" + articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
-
+        //  게시글 정보 입력하면, 게시글 생성한다.
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            //  게시글 수정
+            Article article = articleRepository.getReferenceById(dto.id());
+            if (dto.title() != null) { article.setTitle(dto.title()); }
+            if (dto.content() != null) { article.setContent(dto.content()); }
+            article.setHashtag(dto.hashtag());
+        }catch (EntityNotFoundException e){
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
 
     }
 
     public void deleteArticle(long articleId) {
+        //  삭제
+        articleRepository.deleteById(articleId);
     }
 }
