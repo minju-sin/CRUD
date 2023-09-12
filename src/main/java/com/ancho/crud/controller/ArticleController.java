1a1ac94e2740c47841f61471c2b0a295f332ac6f
chovi <105897857+minju-sin@users.noreply.github.com>
2023-09-12 13:22:11 +0900

    Revert "Merge pull request #20 from minju-sin/feature/#18-sevice-impl"
    This reverts commit 7691c4be889324d6687e88e182019fc24542076d.

--- src/main/java/com/ancho/crud/controller/ArticleController.java
+++ src/main/java/com/ancho/crud/controller/ArticleController.java
@@ -1,19 +1,12 @@
 package com.ancho.crud.controller;
 
-import com.ancho.crud.domain.type.SearchType;
-import com.ancho.crud.dto.response.ArticleResponse;
-import com.ancho.crud.dto.response.ArticleWithCommentsResponse;
-import com.ancho.crud.service.ArticleService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /*
 * /articles
@@ -21,32 +14,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 * /articles/search
 * /articles/search-hashtag
 */
-@RequiredArgsConstructor
+
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
-    private final ArticleService articleService;
-
     @GetMapping
-    public String articles(
-            @RequestParam(required = false)SearchType searchType,
-            @RequestParam(required = false) String searchValue,
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap map
-    ){
-        map.addAttribute("articles",
-                articleService.searchArticles(searchType, searchValue, pageable)
-                        .map(ArticleResponse::from));
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
 
         return "articles/index";
     }
 
     @GetMapping("/{articleId}") //  게시글 아이디를 받음
     public String article(@PathVariable Long articleId, ModelMap map){
-        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
-        map.addAttribute("article", article);
-        map.addAttribute("articleComments", article.articleCommentsResponse());
+        map.addAttribute("article", "article"); // TODO : 구현할 때 여기에 실제 데이터를 넣어줘야 한다.
+        map.addAttribute("articleComments", List.of());
 
         return "articles/detail";
     }
