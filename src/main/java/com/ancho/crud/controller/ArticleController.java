54d02241546891d017334d51c16d39b69ec17feb
minju-sin <sinminju04@gmail.com>
2023-09-15 21:45:43 +0900

    #28 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 `Page` 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 전망

--- src/main/java/com/ancho/crud/controller/ArticleController.java
+++ src/main/java/com/ancho/crud/controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.ancho.crud.domain.type.SearchType;
 import com.ancho.crud.dto.response.ArticleResponse;
 import com.ancho.crud.dto.response.ArticleWithCommentsResponse;
 import com.ancho.crud.service.ArticleService;
+import com.ancho.crud.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /*
  * /articles
@@ -27,7 +31,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
+    //    게시판 페이지
     @GetMapping
     public String articles(
             @RequestParam(required = false)SearchType searchType,
@@ -35,13 +41,16 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ){
-        map.addAttribute("articles",
-                articleService.searchArticles(searchType, searchValue, pageable)
-                        .map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        //  페이지네이션 barNumbers 는 페이지네이션 서비스 안에 존재하는 getPaginationBarNumbers(현재 페이지, 전체 페이지) 함수를 이용해 구할 수 있다.
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
 
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
         return "articles/index";
     }
 
+    //  게시글 페이지
     @GetMapping("/{articleId}") //  게시글 아이디를 받음
     public String article(@PathVariable Long articleId, ModelMap map){
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
